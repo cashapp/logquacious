2c87136b112ca146df5a8c4737ba954b1efab033
Gerald Kaszuba <gkaszuba@squareup.com>
2019-11-26 10:26:39 +1100

    direct link to add filter

--- src/services/log.ts
+++ src/services/log.ts
@@ -244,8 +244,15 @@ export class LogFormatter {
 
             } else if (target.classList.contains('show-context-button')) {
               // We're hijacking the a href when the user left clicks, so that the page doesn't reload.
+              // This will also allow middle clicks to new tabs.
               const parent = target.parentNode as HTMLAnchorElement
               changeQuery(Query.fromURL(getFilters(), parent.href.substring(1)))
+              e.stopPropagation()
+              e.preventDefault()
+
+            } else if (target.classList.contains('filter-link')) {
+              const anchor = target as HTMLAnchorElement
+              changeQuery(Query.fromURL(getFilters(), anchor.href.substring(1)))
               e.stopPropagation()
               e.preventDefault()
             }

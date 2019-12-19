95e32ca10a9715990dc236ef2c531154570d3353
gak <gak@gak0.com>
2019-12-19 16:28:02 +1100

    logo and favicon! (#28)

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,3 +1,5 @@
+import lq from '../../lq.png'
+
 export const Title = () => (
-  <a class="navbar-item" href="./">Logquacious</a>
+  <a class="navbar-item" href="./"><img src={lq}/> Logquacious</a>
 )

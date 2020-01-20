c73cfb4654ffa433fdb4ddc3a5fc0076392ebbc4
gak <gak@gak0.com>
2020-01-20 18:56:42 +1100

    Replace transformer (#75)

--- src/services/log.ts
+++ src/services/log.ts
@@ -456,6 +456,19 @@ function timestamp(): CollapsedTransform {
   }
 }
 
+type ReplaceTransform = {
+  search: string
+  replace: string
+}
+
+function replace(rt: ReplaceTransform): CollapsedTransform {
+  const re = new RegExp(rt.search)
+  return function (input: CollapsedFormatField) {
+    input.current = String(input.current).replace(re, rt.replace)
+    return input
+  }
+}
+
 function mapValue(mapping: Record<string, string>): CollapsedTransform {
   return function (input: CollapsedFormatField) {
     const lookup = mapping[input.current]
@@ -622,6 +635,7 @@ export const collapsedTransformers: { [key: string]: (any) => CollapsedTransform
   mapValue,
   mapClass,
   addClass,
+  replace,
   randomStableColor,
   shortenJavaFqcn,
 }

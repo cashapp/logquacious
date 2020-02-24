e819d869115386a79ee126aa8d9e4275011e9dbb
Gerald Kaszuba <gkaszuba@squareup.com>
2020-02-24 13:59:35 +1100

    lint fix for console.error

--- src/helpers/CopyHelper.ts
+++ src/helpers/CopyHelper.ts
@@ -1,33 +1,34 @@
 import { Lookup } from "./Lookup"
 
 export class CopyHelper {
-    private textarea: HTMLTextAreaElement
+  private readonly textarea: HTMLTextAreaElement
 
-    constructor() {
-        try {
-            this.textarea = Lookup.textarea("#copy-helper")
-        } catch (e) {
-            if (e.message.match("#copy-helper")) {
-              console.error("#copy-helper id not found in dom")
-            } else {
-                throw e
-            }
-        }
+  constructor() {
+    try {
+      this.textarea = Lookup.textarea("#copy-helper")
+    } catch (e) {
+      if (e.message.match("#copy-helper")) {
+        // tslint:disable-next-line:no-console
+        console.error("#copy-helper id not found in dom")
+      } else {
+        throw e
+      }
+    }
+  }
+
+  copy(s: string) {
+    if (!this.textarea) {
+      return
     }
 
-    copy(s: string) {
-        if (!this.textarea) {
-            return
-        }
-
-        const oldFocus = document.activeElement as HTMLElement
-        this.textarea.focus()
-        this.textarea.value = s
-        this.textarea.select()
-        document.execCommand("copy")
-        this.textarea.value = ""
-        if (oldFocus && oldFocus.focus) {
-            oldFocus.focus()
-        }
+    const oldFocus = document.activeElement as HTMLElement
+    this.textarea.focus()
+    this.textarea.value = s
+    this.textarea.select()
+    document.execCommand("copy")
+    this.textarea.value = ""
+    if (oldFocus && oldFocus.focus) {
+      oldFocus.focus()
     }
+  }
 }
